b38d3c7bba117fdba0d279c3c97b749c8b5ad1a4
andreyvolokitin <andreyvlktn@gmail.com>
2020-04-30 17:08:27 +0400

    Imporve scrolling UX, refactor media queries

--- src/js/components/Coworkers.js
+++ src/js/components/Coworkers.js
@@ -1,17 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { nanoid } from 'nanoid/non-secure';
-import { Button } from '@material-ui/core';
+import { Button, IconButton } from '@material-ui/core';
+import { ArrowUpward } from '@material-ui/icons';
+import { debounce } from 'throttle-debounce';
 
 import DataTable from './DataTable';
+import isInViewport from '../util/is-in-viewport';
 
 function Coworkers({ data, datatableStore }) {
   const [id] = useState(nanoid);
   const coworkersData = datatableStore[id] && datatableStore[id].data;
 
+  let containerEl;
+  let headerEl;
   let footerEl;
   const checkDetailsBtn = () => (
-    <Button onClick={scrollToFooter} variant="outlined">
+    <Button className="coworkers__details" onClick={scrollToFooter} variant="outlined" size="small">
       Смотреть
     </Button>
   );
@@ -19,19 +24,52 @@ function Coworkers({ data, datatableStore }) {
   function scrollToFooter() {
     footerEl.scrollIntoView({ behavior: 'smooth' });
   }
+  function scrollToHeader() {
+    headerEl.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  useEffect(() => {
+    const header = headerEl;
+    const container = containerEl;
+    const refresh = debounce(0, () =>
+      // don't use IntersectionObserver due to support for older browsers,
+      // and lack of time to implement 2 solutions
+      container.classList[isInViewport(header) ? 'remove' : 'add']('is-scrolled')
+    );
+
+    window.addEventListener('scroll', refresh);
+    window.addEventListener('resize', refresh);
+    window.addEventListener('orientationchange', refresh);
+    return () => {
+      window.removeEventListener('scroll', refresh);
+      window.removeEventListener('resize', refresh);
+      window.removeEventListener('orientationchange', refresh);
+    };
+  }, []);
 
   /* eslint-disable no-return-assign */
   return (
-    <div className="coworkers">
-      <h3 className="coworkers__head">Список сотрудников</h3>
+    <div ref={(r) => (containerEl = r)} className="coworkers">
+      <h3 ref={(r) => (headerEl = r)} className="coworkers__head">
+        Список сотрудников
+      </h3>
       <DataTable
         tableId={id}
         className="coworkers__data"
         data={coworkersData || data || []}
         headerSecondaryContent={
-          coworkersData && coworkersData.filter((coworker) => coworker.marked).length > 0
-            ? checkDetailsBtn()
-            : ''
+          <>
+            {coworkersData && coworkersData.filter((coworker) => coworker.marked).length > 0
+              ? checkDetailsBtn()
+              : ''}
+            <IconButton
+              className="coworkers__to-top"
+              onClick={scrollToHeader}
+              aria-label="В начало"
+            >
+              <ArrowUpward />
+            </IconButton>
+          </>
         }
       />
       {coworkersData && coworkersData.filter((coworker) => coworker.marked).length > 0 && (
