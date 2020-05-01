9fc53c386e70c8b8a10133d85406db53a77dc1da
andreyvolokitin <andreyvlktn@gmail.com>
2020-05-01 16:29:39 +0400

    Use default component name

--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from '../redux/store';
 
-import CoworkersList from '../components/Coworkers';
+import Coworkers from '../components/Coworkers';
 
 ReactDOM.render(
   <Provider store={store}>
-    <CoworkersList data="/COWORKERS_DATA.json" />
+    <Coworkers data="/COWORKERS_DATA.json" />
   </Provider>,
   document.getElementById('root')
 );
